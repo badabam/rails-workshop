22b95a6e8ac7cd8de585777326f556457b9f8769
Jeremias Dombrowsky <jeremias.dombrowsky@xing.com>
2013-03-26 11:45:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviedb::Application.config.secret_token = '6d4e7bc928290b26cc7fb95238237bcf6e89080eb105074d80f0aaa376a0a6b826947b8a76f260fb79e29fdeeb2c2a9a987580ee6cca0b6216e3e3df205bdf19'
