9b44fafa309257893a52a2949ad398adbc2f1971
Jeremias Dombrowsky <jeremias.dombrowsky@xing.com>
2013-03-26 17:25:14 +0100

    Add missing actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,18 @@
 class MoviesController < ApplicationController
 
+  before_filter :load_movie, :only => [:show, :edit, :update, :destroy]
+
   def index
     @movies = Movie.all
   end
 
   def show
-    @movie = Movie.find(params[:id])
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:id])
     if @movie.update_attributes(params[:movie])
       redirect_to movie_path(@movie)
     else
@@ -23,6 +22,26 @@ class MoviesController < ApplicationController
 
   def new
     @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+private
+
+  def load_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
