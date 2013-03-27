af6e86e9353ef5017826fb7dc10ba0a260e914ce
Jeremias Dombrowsky <jeremias.dombrowsky@xing.com>
2013-03-27 12:33:38 +0100

    Add some genre views. Generate add_genre_to_movies

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,16 @@
 class Genre < ActiveRecord::Base
   attr_accessible :name
+
+  # validates_presence_of :name
+  # validates_uniqueness_of :name
+  # validates_length_of :name, :minimum => 3
+  #
+  # also possible, but does not make sense here:
+  # validates_inclusion_of :name, :in => ['Action', 'Science Fiction']
+
+  validates :name,
+    :presence => true,
+    :uniqueness => true,
+    :length => { :minimum => 3 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326105805) do
+ActiveRecord::Schema.define(:version => 20130327110338) do
+
+  create_table "genres", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "movies", :force => true do |t|
     t.string   "title"
